a503b8b3d09424594cc8cf8e29e9415ee80fc965
Saeed <191458861+SaeedAAli@users.noreply.github.com>
2026-07-05 22:54:30 +0100

    Edited Main.tf from Amazon Linux -> Ubuntu

--- ec2-legacy-app/terraform/main.tf
+++ ec2-legacy-app/terraform/main.tf
@@ -28,14 +28,14 @@ provider "aws" {
   }
 }
 
-# Data source for latest Amazon Linux 2023 AMI
-data "aws_ami" "amazon_linux" {
+
+data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["amazon"]
+  owners      = ["099720109477"]
 
   filter {
     name   = "name"
-    values = ["al2023-ami-*-x86_64"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
@@ -302,7 +302,7 @@ echo "Application setup completed at $(date)" >> /var/log/user-data.log
 
 # EC2 Instance
 resource "aws_instance" "app" {
-  ami                    = data.aws_ami.amazon_linux.id
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.ec2.id]

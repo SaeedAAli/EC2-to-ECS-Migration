0602d59c76d71a31c57a8fc35a54e460a9660ea5
Saeed <191458861+SaeedAAli@users.noreply.github.com>
2026-07-11 00:37:47 +0100

    Edited Main.tf and Provider TF from Bootstrap

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,5 +1,8 @@
 resource "aws_s3_bucket" "ec2toecs" {
   bucket = "ec2toecsmigration"
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_s3_bucket_versioning" "ec2toecs_vers" {
@@ -8,7 +11,5 @@ resource "aws_s3_bucket_versioning" "ec2toecs_vers" {
   versioning_configuration {
     status = "enabled"
   }
-  lifecycle {
-    prevent_destroy = true
-  }
+
 }

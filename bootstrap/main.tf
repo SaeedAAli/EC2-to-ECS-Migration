176751d7026ffae7cdd71a60822ef944e550e841
Saeed <191458861+SaeedAAli@users.noreply.github.com>
2026-07-11 20:18:44 +0100

    Added ID for versioning to bootstrap main.tf

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,12 +1,9 @@
 resource "aws_s3_bucket" "ec2toecs" {
   bucket = "ec2toecsmigration"
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "aws_s3_bucket_versioning" "ec2toecs_vers" {
-  bucket = aws_s3_bucket.ec2toecs
+  bucket = aws_s3_bucket.ec2toecs.id
 
   versioning_configuration {
     status = "enabled"
